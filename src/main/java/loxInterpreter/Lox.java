a4aa5ee4def3dcb3b5c991c3bf7da8509b0d59e5
Joshua Sylvester <jrs63@kent.ac.uk>
2022-04-12 21:36:41 +0100

    Parser completed for expressions

--- src/main/java/loxInterpreter/Lox.java
+++ src/main/java/loxInterpreter/Lox.java
@@ -16,6 +16,8 @@ import java.util.List;
  * @author Josh
  */
 public class Lox {
+    static final boolean DebugLex = false;
+    static final boolean DebugParser = true;
     static boolean hadError = false;
     public static void main (String[] args) throws IOException{
         
@@ -62,18 +64,45 @@ public class Lox {
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
         
-        for(Token token : tokens){
-            System.out.println(token);
+        if(DebugLex){
+            for(Token token : tokens){
+                System.out.println(token);
+            }
         }
+        
+        Parser parser = new Parser(tokens);
+        Expr expression = parser.parse();
+        
+        if(hadError){
+            return;
+        }
+        
+        if(DebugParser){
+            System.out.println(new AstPrinter().print(expression));
+            
+        }
+        
+        
     }
      
-    static void error(int line,String errorMessage){
+    static void error(int line, String errorMessage){
         report(line,"",errorMessage);
     }
     
-    private static void report(int line,String where, String errorMessage){
+    private static void report(int line, String where, String errorMessage){
         System.err.println("[line " + line + "] Error " + where + ": " + errorMessage);
         hadError = true;
+    }
+    
+    static void error(Token token, String message){
+        
+        if(token.type == TokenType.EOF){
+            report(token.line, " at end", message);
+        }
+        else{
+            
+            report(token.line, " at '"+token.lexeme+"'", message);
+        }
     }
     
 }
